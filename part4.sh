b80f7d36be0643dcda0c486b0ac9cb939e2deac7
Evgeniy <anik.evgeniy@gmail.com>
2020-12-02 11:21:16 +0300

    final

--- part4.sh
+++ part4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-awk 'BEGIN{sum=0} {sum+=$1; sum2+=$2; sum3+=$3 } END{print "Сумма по столбцам:" sum, sum2, sum3}' count
+awk 'BEGIN{sum=0} {sum+=$1; sum2+=$2; sum3+=$3; sum4=sum/2; sum5=sum2/2; sum6=sum3/2 } END{print "Сумма по столбцам:" sum, sum2, sum3 ," среднее :" sum4, sum5, sum6 }' count
 
